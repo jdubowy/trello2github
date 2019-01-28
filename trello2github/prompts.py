57a4bb25383ff9391cc0cbdb9608ad5d8eae5559
Joel Dubowy <jdubowy@gmail.com>
2019-01-28 09:28:14 -0800

    Fix bugs in multi-choice prompt code

--- trello2github/prompts.py
+++ trello2github/prompts.py
@@ -1,12 +1,15 @@
 import sys
 
 def multiple_choice(prompt, options):
-    sys.stdout.write(prompt + '\n\n')
+    sys.stdout.write('\n' + prompt + '\n\n')
     sys.stdout.write("Choose one of the following:\n")
     for letter, desc in options:
         sys.stdout.write("  {}) {}\n".format(letter, desc))
 
     x = None
-    while x not in ('s', 'm', 'q'):
+    option_entries = [o[0] for o in options]
+    while x not in option_entries:
         sys.stdout.write("[smq]: ")
         x = input().strip()
+
+    return x
