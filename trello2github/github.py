d61a38f559613e67a6aadfb56da9a0447dc4bd24
Joel Dubowy <jdubowy@gmail.com>
2019-01-28 09:27:26 -0800

    WIP on GitHub issue creation logic

--- trello2github/github.py
+++ trello2github/github.py
@@ -12,14 +12,32 @@ class GitHubClient(object):
 
     def post_issue(self, name, description):
         logging.debug("Posting GitHub issue %s", name)
-        #/repos/:owner/:repo/issues
-        # TODO:
-        #    - prompt user to see if they want to migrate the card (showing all
-        #      text that will be transfered) include options yes, no, quit (exit)
-        #    - if yes:
-        #     - prompt to confirm or edit issue title
-        #     - prompt to confirm or edit issue description
-        #     - post issue to GH
-        #     - post card to GH board (if necessary)
-        #     - move issue to last on project board
-        #     - return url
+        while True:
+            prompt = ("Would you like to post the following issue to Github\n\n"
+                "   " +  name + "\n\n   " + (description or ' (no description) '))
+            options = (
+                ('y', 'yes (post as is)'),
+                ('n', 'no'),
+                ('e', 'edit'),
+                ('q', 'quit (exit)')
+            )
+            x = multiple_choice(prompt, options)
+
+            if x == 'q':
+                logging.info("Good Bye")
+                sys.exit(0)
+            elif x == 'n':
+                return False
+            elif x == 'y':
+                # TODO:
+                #   - post issue to GH - /repos/:owner/:repo/issues
+                #   - post card to GH board (if necessary)
+                #   - move issue to last on project board
+
+                return # TODO: return url
+
+            # else, edit and loop through again
+            sys.stdout.write(" Title: ")
+            name = input().strip()
+            sys.stdout.write(" Description: ")
+            description = input().strip()
